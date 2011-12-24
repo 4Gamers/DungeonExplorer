a642547034832215aa1b958996eb53dc47272623
BBLN <doomclanserv@gmail.com>
2011-12-24 23:26:20 +0200

    Added Blocked tag

--- src/game/Map.cs
+++ src/game/Map.cs
@@ -8,18 +8,17 @@ namespace DungeonExplorer.game
 {
     public class Map
     {
-        [XmlElement(ElementName = "Id")]
         public int Id { get; set; }
 
-        [XmlElement(ElementName = "Name")]
         public string Name { get; set; }
 
-        [XmlElement(ElementName = "Msg")]
         public string Msg { get; set; }
+
+        public int[] Blocked { get; set; }
 
         public bool From(int map)
         {
-            return true; // False if blocked
+            return Blocked.Contains(map); // False if blocked
         }
     }
 
